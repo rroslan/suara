d692d44c6d9c09b51b54db0ad16e3354135576cc
sajadj313 <sajad.jaward1@gmail.com>
2018-12-26 22:55:05 +0530

    code cleanup

--- lib/screens/phone_login.dart
+++ lib/screens/phone_login.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:suara/screens/vendor_settings.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class PhoneLoginScreen extends StatelessWidget {
   final _random = new Random();
