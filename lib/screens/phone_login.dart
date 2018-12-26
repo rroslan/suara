95b2b8ceb444fb2f05eda9a0c3655b6621cc78a2
sajadj313 <sajad.jaward1@gmail.com>
2018-12-26 22:18:03 +0530

    will be removed on future versions

--- lib/screens/phone_login.dart
+++ lib/screens/phone_login.dart
@@ -1,7 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:suara/screens/vendor_settings.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class PhoneLoginScreen extends StatelessWidget {
+  final _random = new Random();
+  final double _latitude;
+  final double _longitude;
+
+  PhoneLoginScreen(this._latitude,this._longitude);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,14 +21,23 @@ class PhoneLoginScreen extends StatelessWidget {
         child: FittedBox(
           child: Column(
             children: <Widget>[
-              Image.asset('images/locked.png',width: 250.0,height: 250.0,),
-              
+              Image.asset(
+                'images/locked.png',
+                width: 250.0,
+                height: 250.0,
+              ),
               RaisedButton(
                 onPressed: () {
-                  var route = MaterialPageRoute(
+                  Navigator.of(context).push(MaterialPageRoute(
                       builder: (BuildContext context) =>
-                          VendorSettingsScreen());
-                  Navigator.of(context).push(route);
+                          VendorSettingsScreen(_latitude,_longitude)));
+                  /*var min = 10000;
+                  var max = 90000;
+                  var generatedCode = min + (_random.nextInt(max-min));
+                  print(generatedCode);*/
+                  /*FirebaseAuth.instance.signInWithPhoneNumber(
+
+                  )*/
                 },
                 child: Text('Login'),
               )
