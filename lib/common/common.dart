bf51f06e1abbb3e46f2b4800ac0812f3b709fbb9
sajadj313 <sajad.jaward1@gmail.com>
2019-01-02 23:15:07 +0530

    introduced to group commonly used methods and increase reusability

---
+++ lib/common/common.dart
@@ -0,0 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> setLocation(double lat, double long) async{
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  pref.setDouble('lat', lat);
+  pref.setDouble('long', long);
+}
+
+Future<dynamic> getLocation() async{
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  var location = {
+    'lat': '${pref.getDouble('lat')}',
+    'long': '${pref.getDouble('long')}'
+  };
+
+  return location;
+}
