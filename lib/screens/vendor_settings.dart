a1e5572b328c1b220d7d6d87e43ef1294951c6b9
sajadj313 <sajad.jaward1@gmail.com>
2019-01-03 22:51:39 +0530

    pulling switch to the right records your location in realtime db

--- lib/screens/vendor_settings.dart
+++ lib/screens/vendor_settings.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:suara/models/vendor_settings.dart';
 import 'package:suara/screens/payment_topup.dart';
 import 'package:flutter_appavailability/flutter_appavailability.dart';
+import 'package:flutter_geofire/flutter_geofire.dart';
 
 class VendorSettingsScreen extends StatefulWidget {
   final double _latitude;
@@ -23,16 +24,23 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
   //var _latTxtController = TextEditingController(text: widget._latitude.toString());
   //var _longTxtController = TextEditingController(text: widget._longitude.toString());
 
-  Future<dynamic> navigateToSettingsPage(String title,initialValue) {
+  Future<dynamic> navigateToSettingsPage(String title, initialValue) {
     return Navigator.of(context).push(MaterialPageRoute(
         fullscreenDialog: true,
-        builder: (BuildContext context) => ChangeVendorSettingPage(title,initialValue)));
+        builder: (BuildContext context) =>
+            ChangeVendorSettingPage(title, initialValue)));
   }
 
   @override
   void initState() {
     super.initState();
     getLoggedInUserDetails();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    String pathToRef = 'sites';
+    Geofire.initialize(pathToRef);
   }
 
   void getLoggedInUserDetails() {
@@ -61,7 +69,11 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
           value: false,
           activeColor: Colors.green,
           inactiveThumbColor: Colors.grey,
-          onChanged: (val) {
+          onChanged: (val) async {
+            print('logged in user Id: ${widget._loggedInUserId}');
+            bool response = await Geofire.setLocation(
+                widget._loggedInUserId, widget._latitude, widget._longitude);
+            print('geofire response: $response');
             _scaffoldKey.currentState.showSnackBar(SnackBar(
               content: Text(val ? 'Online' : 'Offline'),
             ));
@@ -93,7 +105,11 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
                 ? _vendorSettings.businessName
                 : 'Unspecified'),
             onTap: () async {
-              var businessName = await navigateToSettingsPage('Business Name',_vendorSettings.businessName != null ? _vendorSettings.businessName : '');
+              var businessName = await navigateToSettingsPage(
+                  'Business Name',
+                  _vendorSettings.businessName != null
+                      ? _vendorSettings.businessName
+                      : '');
               if (businessName != null) {
                 setState(() {
                   _vendorSettings.businessName = businessName;
@@ -107,8 +123,11 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
                 ? _vendorSettings.businessDesc
                 : 'Unspecified'),
             onTap: () async {
-              var businessDesc =
-                  await navigateToSettingsPage('Business Description',_vendorSettings.businessDesc != null ? _vendorSettings.businessDesc : '');
+              var businessDesc = await navigateToSettingsPage(
+                  'Business Description',
+                  _vendorSettings.businessDesc != null
+                      ? _vendorSettings.businessDesc
+                      : '');
               if (businessDesc != null) {
                 setState(() {
                   _vendorSettings.businessDesc = businessDesc;
@@ -122,7 +141,8 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
                 ? _vendorSettings.fbURL
                 : 'Unspecified'),
             onTap: () async {
-              var fbURL = await navigateToSettingsPage('FB Page URL',_vendorSettings.fbURL != null ? _vendorSettings.fbURL : '');
+              var fbURL = await navigateToSettingsPage('FB Page URL',
+                  _vendorSettings.fbURL != null ? _vendorSettings.fbURL : '');
               if (fbURL != null) {
                 setState(() {
                   _vendorSettings.fbURL = fbURL;
@@ -136,7 +156,11 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
                 ? 'Lat: ${_vendorSettings.location['latitude']}  |  Long: ${_vendorSettings.location['longitude']}'
                 : 'Lat: 0.0000  |  Long: 0.0000'),
             onTap: () async {
-              var location = await navigateToSettingsPage('Location', _vendorSettings.location != null ? '${_vendorSettings.location['latitude']}|${_vendorSettings.location['longitude']}' : null);
+              var location = await navigateToSettingsPage(
+                  'Location',
+                  _vendorSettings.location != null
+                      ? '${_vendorSettings.location['latitude']}|${_vendorSettings.location['longitude']}'
+                      : null);
               if (location != null) {
                 setState(() {
                   _vendorSettings.location = location;
@@ -146,11 +170,17 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
           ),
           ListTile(
             title: Text('Whatsapp Number'),
-            subtitle: Text(_vendorSettings.whatsappNo != null ? _vendorSettings.whatsappNo.isNotEmpty
-                ? _vendorSettings.whatsappNo
-                : 'Unspecified' : 'Unspecified'),
+            subtitle: Text(_vendorSettings.whatsappNo != null
+                ? _vendorSettings.whatsappNo.isNotEmpty
+                    ? _vendorSettings.whatsappNo
+                    : 'Unspecified'
+                : 'Unspecified'),
             onTap: () async {
-              var whatsappNo = await navigateToSettingsPage('Whatsapp No', _vendorSettings.whatsappNo.isNotEmpty ? _vendorSettings.whatsappNo : '');
+              var whatsappNo = await navigateToSettingsPage(
+                  'Whatsapp No',
+                  _vendorSettings.whatsappNo.isNotEmpty
+                      ? _vendorSettings.whatsappNo
+                      : '');
               if (whatsappNo != null) {
                 setState(() {
                   _vendorSettings.whatsappNo = whatsappNo;
@@ -160,11 +190,17 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
           ),
           ListTile(
             title: Text('Phone Number'),
-            subtitle: Text(_vendorSettings.phoneNo != null ? _vendorSettings.phoneNo.isNotEmpty
-                ? _vendorSettings.phoneNo
-                : 'Unspecified' : 'Unspecified'),
+            subtitle: Text(_vendorSettings.phoneNo != null
+                ? _vendorSettings.phoneNo.isNotEmpty
+                    ? _vendorSettings.phoneNo
+                    : 'Unspecified'
+                : 'Unspecified'),
             onTap: () async {
-              var phoneNo = await navigateToSettingsPage('Phone No', _vendorSettings.phoneNo.isNotEmpty ? _vendorSettings.phoneNo : '');
+              var phoneNo = await navigateToSettingsPage(
+                  'Phone No',
+                  _vendorSettings.phoneNo.isNotEmpty
+                      ? _vendorSettings.phoneNo
+                      : '');
               if (phoneNo != null) {
                 setState(() {
                   _vendorSettings.phoneNo = phoneNo;
@@ -175,10 +211,9 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
           ListTile(
             title: Text('Open in Waze'),
             onTap: () {
-
-              AppAvailability.checkAvailability('com.waze').then((appInfo){
+              AppAvailability.checkAvailability('com.waze').then((appInfo) {
                 AppAvailability.launchApp('com.waze');
-              }).catchError((error){
+              }).catchError((error) {
                 _scaffoldKey.currentState.showSnackBar(SnackBar(
                   content: Text(error),
                 ));
@@ -203,7 +238,10 @@ class VendorSettingsScreenState extends State<VendorSettingsScreen> {
                   });
                 });
               },
-              child: Text('Save',style: TextStyle(color: Colors.white),),
+              child: Text(
+                'Save',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           )
         ],
@@ -222,8 +260,12 @@ class ChangeVendorSettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _txt1.text = _appBarTitle.toString() == 'Location'? _initialValue != null ? _initialValue.split('|')[0] : '' : _initialValue;
-    _txt2.text = _appBarTitle.toString() == 'Location'? _initialValue != null ? _initialValue.split('|')[1] : '' : null;
+    _txt1.text = _appBarTitle.toString() == 'Location'
+        ? _initialValue != null ? _initialValue.split('|')[0] : ''
+        : _initialValue;
+    _txt2.text = _appBarTitle.toString() == 'Location'
+        ? _initialValue != null ? _initialValue.split('|')[1] : ''
+        : null;
 
     return Scaffold(
         appBar: AppBar(
@@ -235,10 +277,9 @@ class ChangeVendorSettingPage extends StatelessWidget {
                 style: TextStyle(color: Colors.white),
               ),
               onPressed: () {
-                dynamic returnVal =
-                    _appBarTitle.toLowerCase() == 'location'
-                        ? {'latitude': _txt1.text, 'longitude': _txt2.text}
-                        : _txt1.text;
+                dynamic returnVal = _appBarTitle.toLowerCase() == 'location'
+                    ? {'latitude': _txt1.text, 'longitude': _txt2.text}
+                    : _txt1.text;
                 Navigator.of(context).pop(returnVal);
               },
             )
@@ -247,8 +288,7 @@ class ChangeVendorSettingPage extends StatelessWidget {
         body: ListView(
           children: <Widget>[
             ListTile(
-              title: _appBarTitle.toLowerCase() ==
-                      'business description'
+              title: _appBarTitle.toLowerCase() == 'business description'
                   ? TextField(
                       controller: _txt1,
                       autofocus: true,
