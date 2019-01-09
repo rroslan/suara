6dadb068bb217aa18686678210f6591711e1e318
sajadj313 <sajad.jaward1@gmail.com>
2019-01-09 21:47:44 +0530

    added methods to save switch state in sharedPref

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> setLocation(double lat, double long) async{
+Future<void> setLocation(double lat, double long) async {
   SharedPreferences pref = await SharedPreferences.getInstance();
   pref.setDouble('lat', lat);
   pref.setDouble('long', long);
 }
 
-Future<dynamic> getLocation() async{
+Future<dynamic> getLocation() async {
   SharedPreferences pref = await SharedPreferences.getInstance();
   var location = {
     'lat': '${pref.getDouble('lat')}',
@@ -15,6 +15,20 @@ Future<dynamic> getLocation() async{
   };
 
   return location;
+}
+
+Future<void> setOnlineStatus(bool val) async {
+  var pref = await SharedPreferences.getInstance();
+  await pref.setBool('onlinestatus', val);
+}
+
+Future<bool> getOnlineStatus() async {
+  try {
+    var pref = await SharedPreferences.getInstance();
+    return pref.getBool('onlinestatus');
+  } catch (error) {
+    return null;
+  }
 }
 
 //commonly used for any error message that is given from firebase auth API
