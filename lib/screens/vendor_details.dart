f8cde2bdd332d2a5d8bd36ae079929acd90bcf80
sajadj313 <sajad.jaward1@gmail.com>
2019-01-11 22:56:47 +0530

    designed

--- lib/screens/vendor_details.dart
+++ lib/screens/vendor_details.dart
@@ -1,28 +1,111 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:suara/screens/payment_topup.dart';
+import 'package:suara/models/vendor_settings.dart';
 
+class VendorDetailsScreen extends StatefulWidget {
+  final _loggedInUserId;
 
+  VendorDetailsScreen(this._loggedInUserId);
 
-class VendorDetailsScreen extends StatelessWidget {
-  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+  @override
+  State<StatefulWidget> createState() => VendorDetailsScreenState();
+}
+
+class VendorDetailsScreenState extends State<VendorDetailsScreen> {
+  VendorSettings _vendorDetails;
+
+  @override
+  void initState() {
+    super.initState();
+    var subscription = Firestore.instance
+        .collection('vendorsettings')
+        .document(widget._loggedInUserId)
+        .snapshots()
+        .listen((data) => {});
+
+    subscription.onData((data) {
+      setState(() {
+        _vendorDetails = VendorSettings.fromJson(data.data);
+      });
+      subscription.cancel();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    var businessName = _vendorDetails != null ? _vendorDetails.businessName == null ? 'Please wait...' : _vendorDetails.businessName : 'Please wait...';
+    var businessDesc = _vendorDetails != null ? _vendorDetails.businessDesc == null ? 'Please wait...' : _vendorDetails.businessDesc : 'Please wait...';
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Vendor Details'),
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          CircleAvatar(
-            radius: 50.0,
+          Card(
+            child: ListTile(
+                title: Text(
+                  'Business Details',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 10.0),),
+                    Container(
+                      child: Text('Name',style: TextStyle(
+                        fontSize: 15.0
+                      ),),
+                    ),
+                    Container(
+                      child: Text(businessName),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10.0),),
+                    Container(
+                      child: Text('Description',style: TextStyle(fontSize: 15.0),),
+                    ),
+                    Container(
+                      child: Text(businessDesc),
+                    )
+                  ],
+                )),
           ),
-          Text('Upload Picture'),
-          Text('Business Name'),
-          Text('Business Description'),
-          Text('Facebook Page URL'),
-          Text('Ratings'),
+          Card(
+            child: ListTile(
+                title: Text(
+                  'Social Contacts',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('Call'),
+                      subtitle: Text('Make a phone call'),
+                      trailing: Icon(Icons.phone),
+                    ),
+                    ListTile(
+                      title: Text('SMS'),
+                      subtitle: Text('Send a message'),
+                      trailing: Icon(Icons.sms),
+                    ),
+                    ListTile(
+                      title: Text('Whatsapp'),
+                      subtitle: Text('Open up'),
+                      trailing: Image.asset('images/whatsapp.png',scale: 1.2,),
+                    ),
+                    ListTile(
+                      title: Text('Facebook'),
+                      subtitle: Text('Look up'),
+                      trailing: Image.asset('images/facebook.png',scale: 1.2,),
+                    ),
+                    ListTile(
+                      title: Text('Waze'),
+                      subtitle: Text('Navigate'),
+                      trailing: Image.asset('images/waze.png',scale: 1.2,),
+                    ),
+                  ],
+                )),
+          )
         ],
       ),
     );
